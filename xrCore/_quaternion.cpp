5f40a6ee4d518a2e57773224edb6c2041838ab27
unknown <lunt0360@gmail.com>
2026-06-20 16:41:02 +0700

    restore: fix xrGame link ownership dependencies

--- xrCore/_quaternion.cpp
+++ xrCore/_quaternion.cpp
@@ -2,4 +2,7 @@
 #pragma hdrstop
 
 template XRCORE_API Fquaternion::SelfRef Fquaternion::set(float W, float X, float Y, float Z);
+template XRCORE_API Fquaternion::SelfRef Fquaternion::set(Fquaternion::SelfCRef Q);
+template XRCORE_API Fquaternion::SelfRef Fquaternion::set(const Fmatrix &m);
+template XRCORE_API Fquaternion::SelfRef Fquaternion::rotationYawPitchRoll(float _x, float _y, float _z);
 template XRCORE_API Fquaternion::SelfRef Fquaternion::slerp(Fquaternion::SelfCRef Q0, Fquaternion::SelfCRef Q1, float tm);
