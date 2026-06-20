cc38a78970fb7d59f5d6ef57f0f714d287202201
unknown <lunt0360@gmail.com>
2026-06-20 12:38:01 +0700

    restore: adapt UIStaticItem shader binding calls

--- xr_3da/xrGame/UIStaticItem.cpp
+++ xr_3da/xrGame/UIStaticItem.cpp
@@ -54,7 +54,8 @@ void CUIStaticItem::Init(LPCSTR tex, LPCSTR sh, int left, int top, u32 align)
 void CUIStaticItem::Render		(const ref_shader& sh)
 {
 	// установить обязательно перед вызовом CustomItem::Render() !!!
-	RCache.set_Shader			(sh?sh:hShader);
+	ref_shader		shader		= sh?sh:hShader;
+	RCache.set_Shader			(shader);
 	// convert&set pos
 	Ivector2		bp;
 	HUD().ClientToScreenScaled	(bp,iPos.x,iPos.y,uAlign);
@@ -137,7 +138,8 @@ void CUIStaticItem::Render		(const ref_shader& sh)
 void CUIStaticItem::Render(float angle, const ref_shader& sh)
 {
 	// установить обязательно перед вызовом CustomItem::Render() !!!
-	RCache.set_Shader			(sh?sh:hShader);
+	ref_shader		shader		= sh?sh:hShader;
+	RCache.set_Shader			(shader);
 	// convert&set pos
 	Ivector2		bp;
 	HUD().ClientToScreenScaled	(bp,iPos.x,iPos.y,uAlign);
@@ -162,7 +164,8 @@ void CUIStaticItem::Render(float x1, float y1, float x2, float y2,
 						   float x3, float y3, float x4, float y4, const ref_shader& sh)
 {
 	// установить обязательно перед вызовом CustomItem::Render() !!!
-	RCache.set_Shader		(sh?sh:hShader);
+	ref_shader		shader		= sh?sh:hShader;
+	RCache.set_Shader		(shader);
 	// convert&set pos
 	Ivector2		bp;
 	HUD().ClientToScreenScaled	(bp,iPos.x,iPos.y,uAlign);
