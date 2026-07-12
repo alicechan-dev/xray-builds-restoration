3b11643c1e85ff22ca1ae8413de14240dafd4417
unknown <lunt0360@gmail.com>
2026-07-13 02:32:03 +0700

    restore: enable historical crosshair range readout

--- xr_3da/xrGame/HUDCursor.cpp
+++ xr_3da/xrGame/HUDCursor.cpp
@@ -89,13 +89,11 @@ void CHUDCursor::Render()
 	F->SetAligment	(CGameFont::alCenter);
 	F->SetSize		(0.02f);
 	F->OutSet		(0.f,0.f+di_size*2);
-#ifdef DEBUG
 	if (psHUD_Flags.test(HUD_CROSSHAIR_DIST))
 	{
 		F->SetColor	(C);
 		F->OutNext	("%3.1f",dist);
 	}
-#endif
 
 	if (psHUD_Flags.test(HUD_INFO))
 	{ 
